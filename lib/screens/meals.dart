60eb771c74f0d83aed7170fb01b6f64feaeaf0d0
Anand <anandmishra7946@gmail.com>
2023-09-23 01:41:19 +0530

    Filters and Drawer

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
+    required this.onToggleFavorite,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal){
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal, onToggleFavoite: onToggleFavorite,),),);
+  }
 
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(meals[index].title),
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index], onSelectMeal: (context, meal){
+        selectMeal(context, meal);
+      },),
     );
     if (meals.isEmpty) {
       content = Center(
@@ -43,9 +53,14 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
+    if(title == null){
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
